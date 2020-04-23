ec7dcb4543b57549ddd0a71ce96f00dcd9982c9e
Choudhary <namrata.choudhary@capgemini.com>
2020-04-23 15:29:33 +0530

    adds a python scrip saying hello capsyone project

---
+++ CapstoneNotebook1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook is mainly used to explore IBm data science Capstone Project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
